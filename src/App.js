021e92e1f6a8fcf03dc9b3b5985b6ef94ce7c5f4
Dong Ik Lee <dilee@dnizzle-2.local>
2019-10-08 08:44:49 -0400

    essay_builder: add antd

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { Button } from 'antd';
+import 'antd/dist/antd.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+      <Button type="danger" />
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
